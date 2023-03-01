471fe0ac841822e08ca9e67a593f463a2b012a6a
Mike Solie <mikesolie20@gmail.com>
2023-03-01 17:47:48 -0600

    Added username functionality and updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Password Generator
 
-A password generator that takes user input to output a strong password. 
+A program that takes a first/last name (or two words) to provide a username. Then takes input on desired password length to output username/password combination. 
+
+## Functionality
+
+Username can be adjusted by changing the slices in the username function [:1]. Random and strings are imported to generate random output and reduce lines of code.  
--- passGen.py
+++ passGen.py
@@ -1,14 +1,62 @@
+##########################################
+# Mike Solie                             #
+# 03/01/2023                             #
+# Version 1.2                            #
+#   -->Added Username Function           #
+#                                        #
+# Description:                           #
+# Username Password Generator - Takes    #
+# user input to create a username and    #
+# the desired length of the new password #
+##########################################
+
+#####
+# Import random to randomize characters
+# Import string for simplicity
+#####
 import random
 import string
 
+#####
+# function: username_generator
+# purpose: to create a username
+# inputs: first and last name
+# returns: sliced username
+#####
+def username_generator(first, last):
+    username = first[:1] + last[:5]
+    return username
 
-password_length = int(input('Desired Password Length: '))              # user input for password length
-pass_chars = string.ascii_letters + string.punctuation + string.digits # replaced by string
-new_password = ''                                                      # empty password string
-account = str(input('What is this for? '))                             # Account variable
-for c in range(password_length):                                       # iterate over the password length w/random chars
-	new_password = new_password + random.choices(pass_chars)[0]
+#####
+# function: password generator
+# purpose: create randomized password given user parameters
+# inputs: password length
+# returns: new password
+#####
+def password_generator():
+    new_password = ''
+    password_length = int(input('Desired Password Length: '))
+    pass_chars = string.ascii_letters + string.punctuation + string.digits
+    for c in range(password_length):
+        new_password = new_password + random.choices(pass_chars)[0]
+    return new_password
 
-print(f'The new password for your {account} account is {new_password}')
+#####
+# function: main
+# purpose: to provide new password
+# inputs: names and account
+# returns: nothing
+#####
+def main():
+    name = input('Enter First + Last Name: ').split(' ')
+    user = username_generator(name[0], name[1]).lower()
+    account = str(input('What is this for?: '))
+    password = password_generator()
+    print(f'Your new username is {user}')
+    print(f'Your new password for your {account} account is {password}')
 
+# call to start program
+##---->>
+main()
+##<<----
 
